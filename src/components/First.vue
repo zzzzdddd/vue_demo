01a9343478ee544e77b780a1e120d9df29c8a4dd
zhaoding <18435185303@163.com>
2020-06-16 09:17:39 +0800

    创建项目

---
+++ src/components/First.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="first-app">
+        {{msg}}
+    </div>
+</template>
+
+<script>
+export default {
+    name: "First",
+    data(){
+        return {
+            msg: "Welcome to FirstApp"
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/sub/Confirm.vue
@@ -0,0 +1,1 @@
+import Comfirm f
